1c18b65e25ca67afe73b7b252c64f0b430f85e06
ernebronk <ernebronkhorst@gmail.com>
2017-05-06 00:11:04 +0200

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=gzv',
-    'username' => 'gzv',
-    'password' => 'gzv',
-    'charset' => 'utf8',
-];
